fb8ea5f31f6237ab414c34c312deb632e3d137fa
WTPENGUIN <ccho2932@naver.com>
2023-04-07 09:35:37 +0900

    src: Improve debug messages on the camera stream logic

--- src/comm/LinkManager.cc
+++ src/comm/LinkManager.cc
@@ -466,6 +466,7 @@ void LinkManager::_addZeroConfAutoConnectLink(void)
         if(service.type().startsWith("_mavlink._udp")) {
             static QString udpName("ZeroConf UDP");
             if (checkIfConnectionLinkExist(LinkConfiguration::TypeUdp, udpName)) {
+                qCDebug(LinkManagerVerboseLog) << "Connection already exist";
                 return;
             }
 
@@ -481,6 +482,7 @@ void LinkManager::_addZeroConfAutoConnectLink(void)
         if(service.type().startsWith("_mavlink._tcp")) {
             static QString tcpName("ZeroConf TCP");
             if (checkIfConnectionLinkExist(LinkConfiguration::TypeTcp, tcpName)) {
+                qCDebug(LinkManagerVerboseLog) << "Connection already exist";
                 return;
             }
 
